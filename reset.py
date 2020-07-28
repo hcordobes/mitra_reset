f8ba30d2e37ad16710f67bcb3a4fefa0b2820bc3
hcordobes <hcordobes@gmail.com>
2020-07-29 00:21:47 +0200

    Términos de licencia / LICENSE terms

--- reset.py
+++ reset.py
@@ -1,8 +1,25 @@
 #!/usr/bin/python3
 
-# (c) 2020 @MrHctr
-# Se proporciona sin garantía *de ningún tipo*, no se aceptan
-# responsabilidades por uso *EN NINGÚN CASO*
+"""
+2020 @MrHctr
+
+Exención de responsabilidad
+  Se proporciona sin garantía *de ningún tipo*, no se aceptan
+  responsabilidades por uso *EN NINGÚN CASO*
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
 
 import sys
 import requests
@@ -14,7 +31,7 @@ ip='192.168.1.1'
 password='password'
 
 ####################
-user='1234'
+user = '1234'
 host = 'http://' + ip + '/'
 auth = user + ':' + password
 sessionKey = base64.b64encode(auth.encode())
@@ -23,13 +40,11 @@ r = requests.post(host + 'login-login.cgi', data=data)
 
 # Get cookies (interested in SESSION)
 session = r.cookies
-print(session['SESSION'])
 result = requests.get(host + 'resetrouter.html', cookies=session)
 
 # Extract "var sessionKey='nnnn'"
 
-match=re.search(r"var sessionKey='([0-9]+)'", result.text)
+match = re.search(r"var sessionKey='([0-9]+)'", result.text)
 sessionKey = match.group(1)
-print(sessionKey)
-params={'sessionKey':sessionKey}
+params = {'sessionKey':sessionKey}
 result = requests.get(host + 'rebootinfo.cgi', params=params, cookies=session)
